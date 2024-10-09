12c7fe4bb5894c09dc315811bbc931f2839d0995
Maniyar Sufiyan Ahmed <143694508+sufiyan-ai@users.noreply.github.com>
2024-10-09 20:19:12 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model=joblib.load('INSAID\model.pkl')
+
+st.title('Fraud Detection System')
+
+
+inp1 = int(st.number_input("Enter the Type of Transaction: CASH_OUT: 1, PAYMENT: 2, CASH_IN: 3, TRANSFER: 4, DEBIT: 5 (integer): ",value=0))
+inp2 = float(st.number_input("Enter the Amount of Transaction (float): ",value=0))
+inp3 = float(st.number_input("Enter the Balance Amount of Origin's Old Balance (float): ",value=0))
+inp4 = float(st.number_input("Enter the Balance Amount of Origin's New Balance (float): ",value=0))
+inp5 = float(st.number_input("Enter the Balance Amount of Destination's Old Balance (float): ",value=0))
+inp6 = float(st.number_input("Enter the Balance Amount of Destination's New Balance (float): ",value=0))
+
+
+if st.button("Predict"):
+    inp = np.array([[inp1, inp2, inp3, inp4, inp5, inp6]])
+    
+    #result = model.predict(inp)
+    
+    st.success(f"This Transaction is: {model.predict(inp)}")
